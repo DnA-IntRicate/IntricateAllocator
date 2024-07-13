970d5a9ce495236faa6afcab18c2f3e7c0cd8941
Adam <87928802+DnA-IntRicate@users.noreply.github.com>
2024-07-13 13:51:30 +0200

    Fix infinite loop when making 2 allocations

--- intricate_allocator.c
+++ intricate_allocator.c
@@ -24,7 +24,8 @@ typedef struct heap_chunk_t
 
 struct heap_info_t
 {
-    struct heap_chunk_t* start;
+    heap_chunk_t* start;
+    heap_chunk_t* head;
     size_t avail_size;
 } g_heap_info;
 
@@ -57,6 +58,7 @@ static void ia_heap_init(size_t size)
 
     memset(&g_heap_info, 0, sizeof(struct heap_info_t));
     g_heap_info.start = first;
+    g_heap_info.head = first;
     g_heap_info.avail_size = first->size;
 }
 
@@ -74,8 +76,8 @@ void* ia_alloc(size_t size)
     }
 
     // Traverse the free-list for the first valid large-enough chunk from the head of the heap
-    heap_chunk_t* chunk = g_heap_info.start;
-    while ((chunk != NULL) && (size >= chunk->size) && chunk->in_use)
+    heap_chunk_t* chunk = g_heap_info.head;
+    while ((chunk != NULL) && (size > chunk->size) && chunk->in_use)
         chunk = chunk->next;
 
     if (chunk == NULL)
@@ -96,6 +98,17 @@ void* ia_alloc(size_t size)
         chunk->size = size;
         chunk->next = next_chunk; // Set the next pointer equal to the address of the next chunk.
     }
+    else
+    {
+        // Find the next chunk by advancing the chunk pointer by size
+        heap_chunk_t* next_chunk = (heap_chunk_t*)(((char*)chunk) + sizeof(heap_chunk_t) + size);
+        next_chunk->size = g_heap_info.avail_size - sizeof(heap_chunk_t) - size;
+        next_chunk->in_use = false; // The next chunk should not be in use
+        next_chunk->next = chunk->next; // Set the next chunk pointer of the linked list
+
+        chunk->size = size;
+        chunk->next = next_chunk; // Set the next pointer equal to the address of the next chunk.
+    }
 
     // The current chunk should be marked as in use so that it doesn't get unintentionally overwritten
     // in future calls of this function or before the chunk is freed.
@@ -103,6 +116,7 @@ void* ia_alloc(size_t size)
     printf("Found heap chunk %p of size: %zu bytes\n", chunk, chunk->size);
 
     // Set the head of the heap equal to the next available chunk found previously
+    g_heap_info.head = chunk->next;
     g_heap_info.avail_size -= chunk->size + sizeof(heap_chunk_t);
 
     // Return a pointer to the allocated block (just after the chunk metadata)
@@ -124,11 +138,11 @@ void ia_free(void* block)
 
     heap_chunk_t* chunk = (heap_chunk_t*)((char*)block - sizeof(heap_chunk_t));
     chunk->in_use = false;
-    chunk->next = g_heap_info.start; // Inserting the chunk into the free list
+    chunk->next = g_heap_info.head; // Inserting the chunk into the free list
     // Don't modify chunk sizes yet, eventually this should be done correctly to handle coalescing of chunks
     // chunk->prev_size = chunk->size;
     // chunk->size = 0;
-    g_heap_info.start = chunk;
+    g_heap_info.head = chunk;
     g_heap_info.avail_size += chunk->size + sizeof(heap_chunk_t);
 
     printf("Freed heap chunk %p of size: %zu bytes\n", chunk, chunk->size);
--- main.c
+++ main.c
@@ -24,8 +24,8 @@ int main(int argc, char** argv)
     printf("(1) - Num 1: %d\n", data1->num1);
     printf("(1) - Num 2: %f\n", data1->num2);
 
-    ia_free(data1);
-    printf("Num1: %d\n", data1->num1);
+    ia_free(data1); // data1 has not been overwritten yet therefore its contents are still intact
+    printf("(1)[After Free] - Num1: %d\n", data1->num1);
 
     vec2_t* vec = (my_data_t*)ia_alloc(sizeof(vec2_t));
     vec->x = 2.0f;
@@ -40,17 +40,22 @@ int main(int argc, char** argv)
 
     printf("(2) - Num 1: %d\n", data2->num1);
     printf("(2) - Num 2: %f\n", data2->num2);
-    ia_memset(data2, 0, sizeof(my_data_t));
-    printf("(2)[After ia_memset] - Num 1: %d\n", data1->num1);
-    printf("(2)[After ia_memset] - Num 2: %f\n", data1->num2);
+    data2 = ia_memset(data2, 0, sizeof(my_data_t));
+    printf("(2)[After ia_memset] - Num 1: %d\n", data2->num1);
+    printf("(2)[After ia_memset] - Num 2: %f\n", data2->num2);
 
     // This causes a use-after-free issue since vec is assigned the exact same allocation that data1 was in.
     // When trying to read data1->num1 we just get garbage back from it.
     printf("(1)[After Free] - Num1: %d\n", data1->num1);
 
-    // Using the IA_RELEASE macro it automatically sets the pointer to null to prevent this
+    // Check if vec's data is still intact after allocating data2.
+    printf("Vec:\n");
+    printf("(2) - Vec.x: %f\n", vec->x);
+    printf("(2) - Vec.y: %f\n", vec->y);
+
+    // Using the IA_RELEASE macro it automatically sets the pointer to null to prevent this.
     IA_RELEASE(vec);
-    printf("Vec after IA_RELEASE: %p\n", vec);
+    IA_RELEASE(data2);
 
     (void)getchar();
     return 0;
